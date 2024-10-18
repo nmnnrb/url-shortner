81b18867ad9208b70d4857e29d2cf8910f14f8d6
Naman Sharma <151563433+nmnnrb@users.noreply.github.com>
2024-10-19 01:47:57 +0530

    Update Context.ts

--- frontend/src/helpers/Context.ts
+++ frontend/src/helpers/Context.ts
@@ -1,1 +1,1 @@
-export const serverUrl = import.meta.env.SERVER_URL || "http://localhost:5001/api"
+export const serverUrl = import.meta.env.SERVER_URL || "https://url-shortner-backend-i2ik.onrender.com/api"
