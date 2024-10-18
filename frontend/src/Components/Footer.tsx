6a69b16ab4de608691b0e034cfb4907585921a51
Naman Sharma <151563433+nmnnrb@users.noreply.github.com>
2024-10-19 02:26:31 +0530

    Update Footer.tsx

--- frontend/src/Components/Footer.tsx
+++ frontend/src/Components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer: React.FunctionComponent<IFooterProps> = () => {
   return(
     <>
      <div className="bg-sky-500 mt-2 text-white text-base text-center px-2 py-6">
-        Copyright &#169; URL_Shortner | NamanSharma
+        Copyright &#169; URL_Shortner | <a href="https://www.linkedin.com/in/nmnnrb/"> NamanSharma</a>
      </div>
     </>
   ) ;
